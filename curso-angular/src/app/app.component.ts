6dfaad6dfb4f21351b79f334ca662e4f7d3ae519
FabiMacedo <fabimacedo.oliveira@gmail.com>
2022-11-02 17:58:11 -0300

    Curso Angular #03 - Primeiro Componente

---
+++ curso-angular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Olá Mundo!!! Curso Angular #03 - 02/11/2022';
+}
