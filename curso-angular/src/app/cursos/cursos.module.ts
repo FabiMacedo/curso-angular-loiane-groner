88b84ba785f937cc26807a59b7baf8c41a48f77b
FabiMacedo <fabimacedo.oliveira@gmail.com>
2022-11-02 23:07:15 -0300

    Curso Angular #07: Introdução aos Serviços e Injeção de Dependência

--- curso-angular/src/app/cursos/cursos.module.ts
+++ curso-angular/src/app/cursos/cursos.module.ts
@@ -2,19 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CursosComponent } from './cursos.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
-
-
+import { CursosService } from './cursos.service';
 
 @NgModule({
   declarations: [
     CursosComponent,
     CursoDetalheComponent
   ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    CursosComponent
-  ]
+  imports: [CommonModule],
+  exports: [CursosComponent],
+  providers: [CursosService]
 })
 export class CursosModule { }
